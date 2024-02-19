b4c0d883bd7c36310fdac199cc3bf168206cacbf
Roney <guddorx@gmail.com>
2024-02-19 22:13:07 +0600

    first commit

---
+++ vidly/src/main/java/com/avnthe/vidly/VidlyApplication.java
@@ -0,0 +1,13 @@
+package com.avnthe.vidly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidlyApplication.class, args);
+	}
+
+}
